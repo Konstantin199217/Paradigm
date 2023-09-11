8deaf388ae990e9010023871493f2a6f9e69919c
Konstantin <kot199217@mail.ru>
2023-09-12 00:20:36 +0900

    HW1.1

--- Sem1Hw/src/Main.java
+++ Sem1Hw/src/Main.java
@@ -1,10 +1,5 @@
-import java.awt.*;
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         int[] array = {4,3,2,1,4,2,3};
